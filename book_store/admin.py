2f70cae7e67a9271c9f6b8112a9a5e666dd8d0d6
Nadia HYLARY <nhylary12@gmail.com>
2023-03-07 20:37:04 +0100

    added author model and relations

--- book_store/admin.py
+++ book_store/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author
+
+
 # Register your models here.
 
-admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    # readonly_fields = ("slug", )
+    prepopulated_fields = {"slug": ("title", )}
+    list_filter = ("author", "rating")
+    list_display = ("title", "author", "rating")
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name")
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
--- book_store/models.py
+++ book_store/models.py
@@ -7,13 +7,24 @@ from django.utils.text import slugify
 # Create your models here.
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Name: {self.first_name} {self.last_name}"
+
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    # author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True,
+                               related_name="books")
     is_bestseller = models.BooleanField(default=False)
-    slug = models.SlugField(default=" ", null=False, db_index=True)
+    slug = models.SlugField(default="", blank=True,
+                            null=False, db_index=True)
 
     def __str__(self):
         return f"Title: {self.title} Rating: ({self.rating}) Author: {self.author} Bestseller?: {self.is_bestseller}"
@@ -25,5 +36,7 @@ class Book(models.Model):
         return reverse("book-detail-slug", args=[self.slug])
 
     def save(self, *args, **kwargs):
+        """not really need anymore because the slug field is being
+     automatically prepopulated in the admin interface"""
         self.slug = slugify(self.title)
         super().save(*args, **kwargs)
